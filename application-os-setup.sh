6669770833334bbfa1590003de65c7bb626f7da5
Hamilton S <valhallasoldier@gmail.com>
2025-02-20 15:59:24 -0500

    ajuste script application-os-setup.sh pruebas

--- application-os-setup.sh
+++ application-os-setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 sleep 30
-sudo /usr/bin/firewall-cmd --zone=public --add-port=5000/tcp --permanente
+sudo /usr/bin/firewall-cmd --zone=public --add-port=5000/tcp --permanent
 sudo /usr/bin/firewall-cmd --reload
 sudo cp /home/opc/architecture-ha/bookapp.service /etc/systemd/system/bookapp.service
 sudo systemctl daemon-reload
