f27dfddfaabe1c94efc5ac92c51381fdc2c5d632
Hamilton S <valhallasoldier@gmail.com>
2025-02-21 07:38:23 -0500

    Pruebas SELinux workaround para comandos firewall

--- application-os-setup.sh
+++ application-os-setup.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 sleep 30
-sudo /usr/bin/firewall-cmd --zone=public --add-port=5000/tcp --permanent
-sudo /usr/bin/firewall-cmd --reload
+
+function conf-firewalld() {
+    setenforce 0
+    sudo /usr/bin/firewall-cmd --zone=public --add-port=5000/tcp --permanent
+    sudo /usr/bin/firewall-cmd --reload
+    setenforce 1
+}
+conf-firewalld
 sudo cp /home/opc/architecture-ha/bookapp.service /etc/systemd/system/bookapp.service
 sudo systemctl daemon-reload
 sudo systemctl enable bookapp.service
